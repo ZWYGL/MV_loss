49b87586c76eef28c973bd51e56011fc4ac33ca2
ZWYGL <58805683+ZWYGL@users.noreply.github.com>
2021-08-13 15:33:06 +0800

    Update exp1.sh

--- exp1.sh
+++ exp1.sh
@@ -5,4 +5,4 @@ l3=0.01
 l4=1.0
 mkdir -p ./experiment/metr/$id 
 touch ./experiment/metr/$id/train-$id.log
-CUDA_VISIBLE_DEVICES=3 python3 train.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj  --epoch 100 --expid $id  --save ./experiment/metr/$id --lambda_2 $l2 --lambda_3 $l3 --lambda_4 $l4 --freeze 1 --start 1 --end 5 > ./experiment/metr/$id/train-$id.log
+CUDA_VISIBLE_DEVICES=3 python3 train.py --gcn_bool --adjtype doubletransition --addaptadj  --randomadj  --epoch 100 --expid $id  --save ./experiment/metr/$id --lambda_2 $l2 --lambda_3 $l3 --lambda_4 $l4 --freeze 1 --start 0 --end 12 > ./experiment/metr/$id/train-$id.log
